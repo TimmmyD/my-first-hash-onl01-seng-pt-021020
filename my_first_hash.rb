bc4a5fb13149dfe72abd99495f300e5d5a924c7e
Tim Donoghue <tmd112593@gmail.com>
2020-02-23 23:15:13 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,15 +1,19 @@
 def my_hash
-  my_hash => {
+  my_hash = {"my_basketball" => "spalding"}
 
   
 end
 
 
 def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
+  the_manifest = {"whale bone corsets" => 5,
+                  "porcelain vases" => 2,
+                  "oil paintings" => 3
+                  }
+
+   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
 
 end
 
@@ -19,6 +23,7 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
+  shipping_manifest["oil paintings"]
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
@@ -32,7 +37,7 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-
+  shipping_manifest[]= 
 
   # add 4 gun powder to the shipping_manifest hash below
 
